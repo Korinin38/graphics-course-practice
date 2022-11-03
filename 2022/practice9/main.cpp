6b330e5cd6bb7bbe055158b2b91dae09c73c140f
Korinin38 <artkash@bk.ru>
2022-11-03 16:59:22 +0300

    Practice 9 - Task 6

--- 2022/practice9/main.cpp
+++ 2022/practice9/main.cpp
@@ -89,22 +89,34 @@ void main()
     shadow_pos /= shadow_pos.w;
     shadow_pos = shadow_pos * 0.5 + vec4(0.5);
 
-    vec2 data = texture(shadow_map, shadow_pos.xy).rg;
-    float mu = data.r;
-    float sigma = data.g - mu * mu;
-    float z = shadow_pos.z;
-    float factor = (z < mu) ? 1.0 : sigma / (sigma + (z - mu) * (z - mu));
 
-//    bool in_shadow_texture = (shadow_pos.x > 0.0) && (shadow_pos.x < 1.0) && (shadow_pos.y > 0.0) && (shadow_pos.y < 1.0) && (shadow_pos.z > 0.0) && (shadow_pos.z < 1.0);
-//    float shadow_factor = 1.0;
-//    if (in_shadow_texture)
-//        shadow_factor = (texture(shadow_map, shadow_pos.xy).r + bias < shadow_pos.z) ? 0.0 : 1.0;
+    vec3 sum = vec3(0.0);
+    float sum_w = 0.0;
+    const int N = 5;
+    float radius = 3.0;
+    for (int x = -N; x <= N; ++x)
+    {
+        for (int y = -N; y <= N; ++y)
+        {
+            vec2 data = texture(shadow_map, shadow_pos.xy + vec2(x,y) / textureSize(shadow_map, 0)).rg;
+            float c = exp(-float(x*x + y*y) / (radius*radius));
+            float mu = data.r;
+            float sigma = data.g - mu * mu;
+            float z = shadow_pos.z - bias;
+            sigma += 0.25 * (dFdx(z) * dFdx(z) + dFdy(z) * dFdy(z));
+            float factor = (z < mu) ? 1.0 : sigma / (sigma + (z - mu) * (z - mu));
+            float delta = 0.1;
+            factor = factor < delta ? 0 : (factor - delta) / (1 - delta);
+            sum += c * light_color * max(0.0, dot(normal, light_direction)) * factor;
+            sum_w += c;
+        }
+    }
+
 
     vec3 albedo = vec3(1.0, 1.0, 1.0);
 
     vec3 light = ambient;
-//    light += light_color * max(0.0, dot(normal, light_direction)) * shadow_factor;
-    light += light_color * max(0.0, dot(normal, light_direction)) * factor;
+    light += sum / sum_w;
     vec3 color = albedo * light;
 
     out_color = vec4(color, 1.0);
