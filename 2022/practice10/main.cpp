7058e95882374e2194a32a3a1b18bb43611a7428
Korinin38 <artkash@bk.ru>
2022-11-10 16:18:13 +0300

    Practice 10 - Tasks 3-4

--- 2022/practice10/main.cpp
+++ 2022/practice10/main.cpp
@@ -91,11 +91,17 @@ void main()
 {
     float ambient_light = 0.2;
 
-    float lightness = ambient_light + max(0.0, dot(normalize(normal), light_direction));
+    vec3 bitangent = cross (tangent, normal);
+    mat3 tbn = mat3(tangent, bitangent, normal);
+    vec3 real_normal = tbn * (texture(normal_texture, texcoord).xyz * 2.0 - vec3(1.0));
 
-//    vec3 albedo = texture(albedo_texture, texcoord).rgb;
+//    float lightness = ambient_light + max(0.0, dot(normalize(normal), light_direction));
+    float lightness = ambient_light + max(0.0, dot(normalize(real_normal), light_direction));
+
+    vec3 albedo = texture(albedo_texture, texcoord).rgb;
 //    vec3 albedo = normal * 0.5 + vec3(0.5);
-    vec3 albedo = texture(normal_texture, texcoord).rgb;
+//    vec3 albedo = texture(normal_texture, texcoord).rgb;
+//    vec3 albedo = real_normal * 0.5 + vec3(0.5);
 
     out_color = vec4(lightness * albedo, 1.0);
 }
